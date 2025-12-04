542fe877d5af7cae4a3401790477a99c29411a28
Mohamed Marzouk <mm8479258@gmail.com>
2025-12-04 02:12:20 +0200

    .........

---
+++ lib/Base/Helper/passwordwidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class PasswordWidget extends StatelessWidget {
+  const PasswordWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        decoration: InputDecoration(
+    
+          hint: Text(
+            'كلمة المرور',
+            style: TextStyle(color: Colors.grey[400]),
+          ),
+          label: Text('كلمة المرور'),
+          
+          suffixIcon: Icon(Icons.lock_outline),
+          prefixIcon: Icon(Icons.remove_red_eye_outlined),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(22),
+          ),
+          contentPadding: EdgeInsets.all(24),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(22),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+}
